b7edfa80eee8fc62e54621e481542b32b80fa1e2
Sulman Baig <sulmanweb@gmail.com>
2020-01-21 17:22:30 +0500

    Adding errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Error handling
+  match "/404", to: "error/errors#not_found", via: :all
+  match "/500", to: "error/errors#internal_server_error", via: :all
+
   namespace :v1 do
     namespace :auth do
       post "sign_up", to: "registrations#create"
